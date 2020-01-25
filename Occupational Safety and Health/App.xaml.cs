0703b75f9d84fe5dbd03707e87203d5a19bbf828
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-01-25 14:14:19 +0300

    Add project files.

---
+++ Occupational Safety and Health/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Occupational_Safety_and_Health
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
